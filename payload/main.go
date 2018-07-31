ac2c8a7c1f11babc932bfd88b8b14d6c46fa917a
hydrox <hydroxcookie@protonmail.com>
2018-07-31 00:06:28 -0700

    Added server reconnection.

--- payload/main.go
+++ payload/main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"os/signal"
 	//"strings"
-	//"time"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -17,6 +17,7 @@ var port = flag.String("port", "8080", "Server port.")
 var verbose = flag.Bool("verbose", false, "Additional debuggin logs.")
 
 var CmdChan = make(chan string)
+var Connection *websocket.Conn
 
 func main() {
 	flag.Parse()
@@ -24,25 +25,17 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	host := getServer()
-	debug("Conncting to " + host + "...")
-
-	c, _, err := websocket.DefaultDialer.Dial(host, nil)
-	if err != nil {
-		debug("Dial Error: " + err.Error())
-	}
-
-	defer c.Close()
-
-	done := make(chan struct{})
+	connect()
+	defer Connection.Close()
 
 	go func() {
-		defer close(done)
 		for {
-			_, message, err := c.ReadMessage()
+			_, message, err := Connection.ReadMessage()
 			if err != nil {
 				debug("Read Error: " + err.Error())
-				return // Reconnect in 5 mins...
+
+				connect()
+				defer Connection.Close()
 			}
 			debug(fmt.Sprintf("Incoming: %s\n", message))
 		}
@@ -50,19 +43,13 @@ func main() {
 
 	for {
 		select {
-		case <-done:
-			return
 		case <-interrupt:
 			debug("INTERRUPT!")
 
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			err := Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				debug("Write Close Error: " + err.Error())
 				return
-			}
-
-			select {
-			case <-done:
 			}
 
 			return
@@ -82,4 +69,25 @@ func debug(message string) {
 
 func getServer() string {
 	return "ws://" + *server + ":" + *port + "/ready"
+}
+
+func connect() {
+	host := getServer()
+
+	debug("Conncting to " + host + "...")
+
+	for {
+		c, _, err := websocket.DefaultDialer.Dial(host, nil)
+		if err == nil {
+			debug("Connection established!")
+			Connection = c
+			return
+		}
+
+		if *verbose == true {
+			debug("Connection Error: " + err.Error())
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
